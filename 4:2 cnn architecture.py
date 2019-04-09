e0e78c41072e86ba53e402b632b35b71ddca3fb8
Suvhradip Ghosh <44000819+suvhradipghosh07@users.noreply.github.com>
2019-04-09 11:49:16 +0530

    4:2 cnn architecture on Chest Analysis dataset

---
+++ 4:2 cnn architecture.py
@@ -0,0 +1,52 @@
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, Convolution2D
+from keras.layers import Dropout
+from keras.layers.normalization import BatchNormalization
+from keras import optimizers
+import warnings
+warnings.filterwarnings("ignore")
+
+output_dim=10
+batch_size = 128 
+nb_epoch = 50
+input_shape = (64, 64, 3)
+#Initialize model
+model = Sequential()
+
+#CNN Layer 1
+#input: 28X28 images with 1 channel -> (28X28X1) tensors.
+#this applies 64 convolution filters of size 3X3 each.
+model.add(ZeroPadding2D((1,1), input_shape=input_shape))
+model.add(Convolution2D(filters=64, kernel_size=(3,3), activation='relu', kernel_initializer='he_normal'))
+model.add(Dropout(0.4))
+
+#CNN Layer 2
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(filters=64, kernel_size=(3,3), activation='relu', kernel_initializer='he_normal'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+model.add(Dropout(0.4))
+
+#Flatten the output before feeding these to the fully connected Neurons                                    
+model.add(Flatten())
+
+#Fully connected Layers
+model.add(Dense(256, activation='relu', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+model.add(Dense(128, activation='relu', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid', kernel_initializer='he_normal'))
+
+#Using adam as our optimzer
+sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
+#Get a summary of the model.
+print(model.summary())
+
+#Compile the model
+model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
